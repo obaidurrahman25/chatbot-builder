4145aaf6e3de27f4b9d48fdadb3ab40500c6b8e1
Obaidur Rahman <obaidurrahman2501@gmail.com>
2024-06-11 17:09:04 +0530

    Basic ReactFlow setup

--- src/pages/EditWindow/EditWindow.js
+++ src/pages/EditWindow/EditWindow.js
@@ -1,11 +1,21 @@
 import React, { useCallback } from 'react';
 import './EditWindow.scss';
+import Header from './../../components/Header/Header';
+import Sidebar from './../../components/Sidebar/Sidebar';
+import EditingArea from './../../components/EditingArea/EditingArea';
+import SettingsWindow from './../../components/SettingsWindow/SettingsWindow';
+
 
 function EditWindow() {
 
   return (
     <div className="edit-window-container">
-      
+      <Header/>
+      <div className='page-content'>
+        <Sidebar/>
+        <EditingArea/>
+        <SettingsWindow/>
+      </div>
     </div>
   );
 }
